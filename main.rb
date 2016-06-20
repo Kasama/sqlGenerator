4c7530fdf69e4dc08198621b38ed7107975b7055
Kasama <ralegro@bol.com.br>
2016-06-20 13:13:01 -0300

    added command line arguments

--- main.rb
+++ main.rb
@@ -12,23 +12,27 @@ def erbfy(filename, erb_params)
 	ERB.new(File.read(filename).gsub(/^\s+/, ''), nil, '><%').result binding
 end
 
-config = Config.new(ENV['file'])
+if ARGV.size != 3
+	STDERR.puts "Usage: ${$0} <schema.yml.erb> <output_folder> <java_package>"
+end
+
+config = Config.new(ARGV[0])
 
 tab = parse(config)
 
-output_folder = 'out'
+output_folder = ARGV[1]
 sql_name = 'schema.sql'
+print "Creating file #{sql_name}..."
 File.open("#{output_folder}/#{sql_name}", "w+") do |f|
-	print "Creating file #{sql_name}..."
 	f.write(erbfy('template.sql.erb', tab))
-	puts "Done"
 end
+puts "Done"
 
 tab.each do |table_name, table|
 	current_java = "#{capitalize(table_name)}.java"
 	print "Creating file #{current_java}..."
 	File.open("#{output_folder}/#{current_java}", "w+") do |f|
-		params = {table: table, package: "br.usp.icmc.paralimpiadas"}
+		params = {table: table, package: ARGV[2]}
 		f.write(erbfy('template.java.erb', params))
 	end
 	puts "Done"
